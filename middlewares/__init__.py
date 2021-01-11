41107667adecc9c3f91ecc02b3899f42a735f061
Like6po <42671486+Like6po@users.noreply.github.com>
2021-01-11 18:15:06 +0300

    change_middlewares

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,9 +1,7 @@
 from aiogram import Dispatcher
 
 from .throttling import ThrottlingMiddleware
-#from .data import DataMiddleware
 
 
 def setup(dp: Dispatcher):
     dp.middleware.setup(ThrottlingMiddleware())
-    #dp.middleware.setup(DataMiddleware())
