55869bc473b83560fc231c0b03b1c74f3324b740
Like6po <42671486+Like6po@users.noreply.github.com>
2021-01-11 18:47:06 +0300

    add_links

--- keyboards/inline/start.py
+++ keyboards/inline/start.py
@@ -7,10 +7,14 @@ def start_kb():
     kb_obj = InlineKeyboardMarkup()
     key_add_to_chat = InlineKeyboardButton(text='🤵🏻 Добавить в чат',
                                            url='http://t.me/brawl_mafia_bot?startgroup=True')
+    key_news = InlineKeyboardButton(text='📝 Новости бота',
+                                    url='http://t.me/brawl_mafia_news')
+    key_chat = InlineKeyboardButton(text='👨‍🦰 Чат для игры',
+                                    url='http://t.me/brawl_mafia_chat')
     key_help = InlineKeyboardButton(text='💬 Помощь',
                                     callback_data=start_callback.new(type='help'))
-    kb_obj.row(key_add_to_chat)
-    kb_obj.row(key_help)
+    kb_obj.row(key_add_to_chat, key_news)
+    kb_obj.row(key_help, key_chat)
     return kb_obj
 
 
