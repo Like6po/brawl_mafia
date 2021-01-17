7accb435600beafaa83f77b9ea24b7f62544724a
Like6po <42671486+Like6po@users.noreply.github.com>
2021-01-17 16:26:48 +0300

    rename /profile file

--- handlers/groups/__init__.py
+++ handlers/groups/__init__.py
@@ -5,7 +5,7 @@ from .play import dp
 from .close import dp
 from .settings import dp
 from .leave import dp
-from .other import dp
+from .profile import dp
 from .left_chat_member import dp
 from .new_chat_member import dp
 from .any_messages import dp
